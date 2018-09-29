e226ef9fed966b900b50ce5dde3c2759ee7d845d
RemModz <rem.modz@gmail.com>
2018-09-29 14:50:51 +0200

    zadanie nr2

--- js/app.js
+++ js/app.js
@@ -1,7 +1,7 @@
 'use strict';
 
 document.addEventListener('DOMContentLoaded', function() {
- var menuButton = document.querySelector('.for-dropdown');
+var menuButton = document.querySelector('.for-dropdown');
 var subMenu = document.querySelector('.dropdown');
     
     menuButton.addEventListener('mouseover', function() {
@@ -11,4 +11,27 @@ var subMenu = document.querySelector('.dropdown');
     menuButton.addEventListener('mouseout', function() {
         subMenu.style.display = 'none';
       });
+    
+    //zadanie 2 
+    
+    
+    function showHide(){
+                var textArea = this.previousElementSibling;
+        
+        if (textArea.style.display === 'none' || textArea.style.display === '') {
+          textArea.style.display = 'block';
+            this.innerHTML = 'MNIEJ <span class="glyphicon glyphicon-chevron-up"></span>';
+        } else {            
+        textArea.style.display = 'none';
+        this.innerHTML = 'WIĘCEJ <span class="glyphicon glyphicon-chevron-down"></span>';
+        }
+    }
+    
+    var buttons = document.querySelectorAll('.read-more');
+    
+ //   buttons[0].addEventListener('click', showhide);
+
+        for (var i = 0; i < buttons.length; i++) {
+        buttons[i].addEventListener('click', showHide);
+    }
 });
